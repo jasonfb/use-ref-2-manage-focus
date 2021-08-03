04982d67488ca3821871d02b1618e63fef69ac3c
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2021-08-03 18:08:06 -0400

    weather app with zip code loader

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,12 +1,52 @@
-import React,  {useState, useRef} from 'react'
-
+import React,  {useState, useRef, useEffect} from 'react'
 
 
 const WeatherApp = (props) => {
   const [zip, setZip] = useState(undefined);
+  const [forecast, setForecast] = useState("")
   const zipCodeRef  = useRef();
 
   const weather_forecast = "";
+
+  const OPEN_WEATHER_MAP_API_KEY = "f1fabb26c7c42ec9e4e5c7696e46b32f"
+
+  const fetchForecast = (zipCode) => {
+    fetch(`//api.openweathermap.org/data/2.5/forecast?zip=${zip}&appid=${OPEN_WEATHER_MAP_API_KEY}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+
+          if (Number(result.cod) === 200) {
+
+            let k_temp = result.list[0].main.temp;
+
+            let f_temp = Math.trunc((k_temp - 273.15 ) * 9/5 + 32)
+            let fc = `Currently ${f_temp} °F: ${ result.list[0].weather[0].description}`;
+
+            console.log(fc)
+            setForecast(fc)
+
+          } else {
+            setForecast(result.message)
+
+          }
+        }
+      )
+  }
+
+
+  useEffect(() => {
+    fetchForecast(zipCodeRef.current.value)
+
+  }, [zip])
+
+  const handleKeyPress = (event) => {
+
+    if (zipCodeRef.current)  {
+      setZip(zipCodeRef.current.value)
+    }
+  }
 
   return (
     <>
@@ -16,13 +56,15 @@ const WeatherApp = (props) => {
 
     <br />
       Input your zipcode:
-      
+
       <input name={"zip"}
              ref={zipCodeRef}
-             value={zip}/>
+             onChange={handleKeyPress}
+
+      />
 
       <br />
-      {weather_forecast}
+      {forecast}
 
     <br/>
     </>
